ebe89bb8e7984afe2926ad4060efc1401b665a05
kimingw <m84013885@163.com>
2025-03-28 12:23:34 +0800

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+// import ReactSlideVerify from 'react-slide-verify-mw'
 
 createRoot(document.getElementById('root')!).render(
   <App
     getUrl='http://localhost:3000/api/slide-challenge'
     checkUrl='http://localhost:3000/api/slide-verify'
-    onSuccess={(token) => {
+    onSuccess={(token: string) => {
       console.log('验证成功 ✓', token)
     }}
   />
