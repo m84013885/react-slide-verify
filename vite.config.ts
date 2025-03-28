bae51f592898d2a764c09a46bd73a63083e3902e
kimingw <m84013885@163.com>
2025-03-28 11:47:19 +0800

    feat : build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync, existsSync } from 'fs'
+import path from 'path'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    cssInjectedByJsPlugin(),
+    {
+      name: 'copy-dts',
+      closeBundle() {
+        const src = 'src/react-select-country.d.ts';
+        const dest = 'dist/react-select-country.d.ts';
+        if (existsSync(src)) {
+          copyFileSync(src, dest);
+        } else {
+          // console.error(`Error: ${src} does not exist.`);
+        }
+      }
+    }
+  ],
+  css: {
+    postcss: {
+      plugins: [],
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/App.tsx'),
+      name: 'react-select-country-mw',
+      fileName: (format) => `react-select-country.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    },
+    cssCodeSplit: false
+  }
 })
